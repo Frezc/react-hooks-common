1cc9a94a90e0c5aba7c362b8acf1ac84cffce2c4
wanli <wanli@qunhemail.com>
2019-02-26 15:00:09 +0800

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { default as usePromise } from 'react-use-promise';
+import usePromiseRaw from 'react-use-promise';
+export function usePromise<Result = any>(promise: Promise<Result> | (() => Promise<Result>), deps?: any[] | undefined) {
+  const [result, error, state] = usePromiseRaw(promise, deps);
+  return [state, result || undefined, error];
+}
