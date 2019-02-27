0d1eebb8156684a3e2dc020339ce05748ef7b92b
wanli <wanli@qunhemail.com>
2019-02-27 14:33:17 +0800

    0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import usePromiseRaw from 'react-use-promise';
-export function usePromise<Result = any>(promise: Promise<Result> | (() => Promise<Result>), deps?: any[] | undefined) {
+export function usePromise<Result = any>(promise: Promise<Result> | (() => Promise<Result>), deps?: any[] | undefined): ["pending" | "resolved" | "rejected", Result | undefined, Error] {
   const [result, error, state] = usePromiseRaw(promise, deps);
   return [state, result || undefined, error];
 }
