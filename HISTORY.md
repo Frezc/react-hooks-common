d02e7f185d2dcd74eef203de425f16c9b613ca70
wanli <wanli@qunhemail.com>
2019-08-06 14:53:45 +0800

    upgrade 0.3.6

--- HISTORY.md
+++ HISTORY.md
@@ -5,6 +5,9 @@ order: 99
 
 # HISTORY
 
+## 0.3.6
+- upgrade react-use-promise-alternative@0.2.1-alt: allow non-promise in usePromise.
+
 ## 0.3.5
 - upgrade use-lodash-debounce-throttle@0.3.5 to fix type and upgrade lodash-es
 
--- src/use-promise.ts
+++ src/use-promise.ts
@@ -1,5 +1,5 @@
 import usePromiseRaw from 'react-use-promise-alternative';
-export default function usePromise<Result = any>(promise: Promise<Result> | (() => (Promise<Result> | void)), deps?: any[]): ["pending" | "resolved" | "rejected", Result | undefined, Error] {
+export default function usePromise<Result = any>(promise: Promise<Result> | Result | (() => (Promise<Result> | Result)), deps?: any[]): ["pending" | "resolved" | "rejected", Result | undefined, Error] {
   const [result, error, state] = usePromiseRaw(promise as any, deps);
   return [state, result || undefined, error];
 }
